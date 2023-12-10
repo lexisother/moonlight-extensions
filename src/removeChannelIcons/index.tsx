4ab9ac72d05719d60d4440ef832d239f8ac7f888
Alyxia Sother <alyxia@riseup.net>
2023-12-10 15:50:23 +0100

    [removeChannelIcons] Fix patch

--- src/removeChannelIcons/index.tsx
+++ src/removeChannelIcons/index.tsx
@@ -1,25 +1,11 @@
 import { Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
-  // {
-  //   find: /iconEmoji/,
-  //   replace: {
-  //     match: /return{iconEmoji:(.*?)}/,
-  //     replacement: (_) => `return{iconEmoji:null}`,
-  //   },
-  // },
-  // {
-  //   find: /\.emojiColorFill/,
-  //   replace: {
-  //     match: /className:.{1,2}\(\)\.emojiColorFill/,
-  //     replacement: (_) => `className:''`,
-  //   },
-  // },
   {
-    find: /useChannelEmojiAndColor:/,
+    find: 'ChannelItemIcon:',
     replace: {
-      match: /return{emoji:(.*?),color:(.*?)}/g,
-      replacement: () => `return{emoji:null,color:null}`
+      match: /(?<=useChannelEmojiAndColor\)\(.+?\),.+?=).+?null/, // ty rosalie
+      replacement: 'null',
     }
   }
 ];
